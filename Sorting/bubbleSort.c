8a0d5ec41a79ed540a866684f7c73bf2d99b0234
sakibhossain323 <sakibhossain323@gmail.com>
2025-06-22 21:49:52 +0600

    optimize bubble sort

--- Sorting/bubbleSort.c
+++ Sorting/bubbleSort.c
@@ -5,16 +5,20 @@ void bubbleSort(int arr[], int n)
 {
     for(int i = 0; i < n - 1; i++)
     {
+        int swapped = 0;
         for(int j = 0; j < n - i - 1; j++)
         {
             if(arr[j] > arr[j + 1])
             {
-                // Swap arr[j] and arr[j + 1]
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+            // Swap arr[j] and arr[j + 1]
+            int temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+            swapped = 1;
             }
         }
+        if(!swapped)
+            break;
     }
 }
 
